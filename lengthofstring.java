84195d79e7315de4f5b985dff1a9677804f3fa42
PMorey22 <110521793+PMorey22@users.noreply.github.com>
2023-02-07 23:16:04 +0530

    Add files via upload

---
+++ lengthofstring.java
@@ -0,0 +1,7 @@
+public class lengthofstring {
+    public static void main(String args[]) {
+        String a = new String();
+        a = "Hello";
+        System.out.println(a.length());
+    }
+}
