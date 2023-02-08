12d2b3e68a33c1fc10ffbaf87f6621be7a933f95
PMorey22 <110521793+PMorey22@users.noreply.github.com>
2023-02-08 23:26:33 +0530

    Add files via upload

---
+++ Hollowpattern.java
@@ -0,0 +1,34 @@
+package Pattern;
+
+public class Hollowpattern {
+
+    public static void main(String args[]) {
+        // for (int i = 0; i < 4; i++) {
+        // for (int j = 0; j < 5; j++) {
+        // if (i == 0 || i == 3) {
+        // System.out.print("*");
+        // } else {
+        // if (j == 0 || j == 4) {
+        // System.out.print("*");
+        // } else {
+        // System.out.print(" ");
+        // }
+        // }
+        // }
+        // System.out.println();
+        // }
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (i == 0 || i == 3 || j == 0 || j == 4) {
+                    System.out.print("*");
+
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
