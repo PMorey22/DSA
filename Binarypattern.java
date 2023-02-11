aaa1db435789ceee3e8659e099351ada919ecf49
PMorey22 <110521793+PMorey22@users.noreply.github.com>
2023-02-11 23:36:08 +0530

    Add files via upload

---
+++ Binarypattern.java
@@ -0,0 +1,21 @@
+package Pattern;
+
+public class Binarypattern {
+    public static void main(String args[]) {
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+
+            }
+
+            System.out.println();
+        }
+
+    }
+
+}
